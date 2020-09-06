91640dae5d0c4807a6e5fa4f25d4e25fdb5325a4
Lucas Fernandez <lucas.236598@nv.ccsd.net>
2020-09-06 18:07:59 +0000

    Completed all 4 labs

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -7,12 +7,17 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        
-
+        int class1 = scanner.nextInt();
+        int class2 = scanner.nextInt();
+        int class3 = scanner.nextInt();
         /*
          *  your code goes here
          */
+        int desksNeeded = ((class1 / 2) + (class2 / 2) + (class3 / 2));
+        desksNeeded += ((class1 % 2) + (class2 % 2) + (class3 % 2));
 
+        System.out.print(desksNeeded);
+        
 
         // closing the scanner object
         scanner.close();
